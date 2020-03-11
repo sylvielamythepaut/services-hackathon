6480a6f8b49ad7afa67a72727e443e0888639b2b
James Varndell <james.varndell@ecmwf.int>
2020-03-11 16:09:05 +0000

    First draft of plot_slice.py

--- plot-slice/plot_slice.py
+++ plot-slice/plot_slice.py
@@ -6,14 +6,74 @@
 ### The code is auto generated, your change will be overwritten by 
 ### code generating.
 ###
-from __future__ import absolute_import
+
+import os
+from Magics.macro import *
 
 
 
 
+def plotslice(context, source):
+    source = context.get_data(source)
 
-def plotslice(context, *args):
-    return {}
+    result = context.create_result('.png')
+
+    output = output(   
+                output_formats = ['png'],
+                output_name = os.path.basname(result.path),
+                output_name_first_page_number = "off"
+        )
+    # Setting the cartesian view
+    projection = mmap(
+        subpage_map_projection='cartesian',
+        subpage_x_axis_type='geoline',
+        subpage_y_axis_type='logarithmic',
+        subpage_x_min_latitude=50.,
+        subpage_x_max_latitude=30.,
+        subpage_x_min_longitude=-90.,
+        subpage_x_max_longitude=-60.,
+        subpage_y_min=1020.,
+        subpage_y_max=200.,
+        )
+    # Vertical axis
+    vertical = maxis(
+        axis_orientation='vertical',
+        axis_grid='on',
+        axis_type='logarithmic',
+        axis_tick_label_height=0.4,
+        axis_tick_label_colour='charcoal',
+        axis_grid_colour='charcoal',
+        axis_grid_thickness=1,
+        axis_grid_reference_line_style='solid',
+        axis_grid_reference_thickness=1,
+        axis_grid_line_style='dash',
+        axis_title='on',
+        axis_title_text='Pressure',
+        axis_title_height=0.6,
+        )
+    # Horizontal axis
+    horizontal = maxis(
+        axis_orientation='horizontal',
+        axis_type='geoline',
+        axis_tick_label_height=0.4,
+        axis_tick_label_colour='charcoal',
+        axis_grid='on',
+        axis_grid_colour='charcoal',
+        axis_grid_thickness=1,
+        axis_grid_line_style='dash',
+        )
+    # Definition of the netCDF data and interpretation
+    data = mnetcdf(netcdf_filename = "section.nc",
+      netcdf_value_variable = "p13820121030000000000001",
+      netcdf_field_scaling_factor = 100000.,
+      netcdf_y_variable = "levels",
+      netcdf_x_variable = "longitude",
+      netcdf_x_auxiliary_variable = "latitude"
+    )
+    contour = mcont()
+    plot(output, projection, horizontal, vertical, data, contour)    
+
+    return result
     
 
 
