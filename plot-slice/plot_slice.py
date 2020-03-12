d2863c5cd2a9305c6a336b657082a56cf544fbf0
James Varndell <james.varndell@ecmwf.int>
2020-03-12 10:58:54 +0000

    service version of plot_slice.py

--- plot-slice/plot_slice.py
+++ plot-slice/plot_slice.py
@@ -11,28 +11,32 @@ import os
 from Magics.macro import *
 import netCDF4 as nc
 
-title = {
-        "text_font_size": 2., ""
+TITLE = {
+        "text_font_size": 1.1, "text_colour": 'black',
+        #"text_line_space_ratio": 0.1
         }
 
-style = { 
-        "legend": "on",
+LEGEND = {
+        "legend_title": "on",
         "legend_text_colour": "black",
-        "legend_title": "Atmospheric cross-section of temperature (K)",
-        "contour": "off",
-        "contour_label": "off",
-        "contour_shade": "on",
-        "contour_shade_method" : "area_fill",
+        "legend_title_text": "Atmospheric cross-section of temperature (K)",
+        "legend_title_font_size": 0.6,
         "legend_entry_border": "off",
         "legend_values_list": [200.,225,250,275,300],
         "legend_text_composition": "user_text_only",
         "legend_display_type": 'continuous',
+        }
+CONTOUR = { 
+        "legend": "on",
+        "contour": "off",
+        "contour_label": "off",
+        "contour_shade": "on",
+        "contour_shade_method" : "area_fill",
         "contour_level_selection_type": "list",
         "contour_shade_colour_method": "gradients",
         "contour_level_list": [-1e6,200,225,250,300,1e6],
         "contour_gradients_colour_list" : ["#30123b","#30123b","#18ddc2","#fbb637","#850702"],
         "contour_gradients_step_list": [1,25,25,50,1],
-        "contour_description" : "Turbo based continuous pallete with range 0 to 400"
       }
 
 def plotslice(context, source, variable):
@@ -117,17 +121,17 @@ def plotslice(context, source, variable):
         )
     print(horizontal)
     
-    tit = mtext(
-            **title
+    title = mtext(
+            **TITLE
             )
     contour = mcont(
-            #contour_automatic_setting = 'style_name',
-            #contour_style_name        = "sh_blured_f0t300",
-            #legend                    = 'on'
-            **style
+            **CONTOUR
+            )
+    legend = mlegend(
+            **LEGEND
             )
     plot(output_inst, projection, horizontal, vertical, data, contour,
-            tit)
+            title, legend)
 
     return result
     
